8a29beaebc3363b25f8ad2591e1a559244b47792
Rafael Mota <rafael.mota@ufca.edu.br>
2024-09-24 10:45:54 -0300

    feat: detecção apenas na região de interesse

--- object_tracking.py
+++ object_tracking.py
@@ -9,15 +9,14 @@ import numpy as np
 from object_detection import ObjectDetection
 import math
 
+CUT_IMAGE = 3 #Quanto menor, menos corta a imagem. Ex: 2 corta pela metade. 3 corta 1/3 da imagem.
+CUT_IMAGE_V = 1
+DISTANCIA = 80
+
 # Initialize Object Detection
 od = ObjectDetection()
-<<<<<<< HEAD
 
-cap = cv2.VideoCapture("los_angeles.mp4")
-=======
-print(od.classes)
-cap = cv2.VideoCapture("cars2.mp4")
->>>>>>> e14c08a... agora sim
+cap = cv2.VideoCapture("cars.mp4")
 
 # Initialize count
 count = 0
@@ -34,26 +33,30 @@ while True:
 
     # Point current frame
     center_points_cur_frame = []
-
+    if CUT_IMAGE > 1:
+        frame = frame[frame.shape[0]//CUT_IMAGE:frame.shape[0]]
+    if CUT_IMAGE_V > 1:
+        frame = frame[:,frame.shape[1]//CUT_IMAGE_V:]
     # Detect objects on frame
     (class_ids, scores, boxes) = od.detect(frame)
-    for box in boxes:
+    for box,class_id in zip(boxes, class_ids):
         (x, y, w, h) = box
         cx = int((x + x + w) / 2)
         cy = int((y + y + h) / 2)
-        center_points_cur_frame.append((cx, cy))
-        #print("FRAME N°", count, " ", x, y, w, h)
-
-        # cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+        if (cy > frame.shape[0]//3) and (cy < (frame.shape[0]*2)//3):
+            center_points_cur_frame.append((cx, cy))
+            #print("FRAME N°", count, " ", x, y, w, h)
+            #cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #print(class_id,od.classes[class_id])
+    
     # Only at the beginning we compare previous and current frame
     if count <= 2:
         for pt in center_points_cur_frame:
             for pt2 in center_points_prev_frame:
                 distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
 
-                if distance < 20:
+                if distance < DISTANCIA:
                     tracking_objects[track_id] = pt
                     track_id += 1
     else:
@@ -67,8 +70,11 @@ while True:
                 distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
 
                 # Update IDs position
-                if distance < 20:
+                if distance < DISTANCIA:
                     tracking_objects[object_id] = pt
+                    #if (pt2[1] > frame.shape[0]//3) and (pt2[1] < (frame.shape[0]*2)//3):
+                        #if (pt[1]-pt2[1]) < 0:
+                        #    cv2.putText(frame, "Subindo...", (pt[0], pt[1] - 20), 0, 1, (0, 0, 255), 2)
                     object_exists = True
                     if pt in center_points_cur_frame:
                         center_points_cur_frame.remove(pt)
@@ -84,16 +90,16 @@ while True:
             track_id += 1
 
     for object_id, pt in tracking_objects.items():
-        cv2.circle(frame, pt, 5, (0, 0, 255), -1)
-        cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
+        if (pt[1] > frame.shape[0]//3) and (pt[1] < (frame.shape[0]*2)//3):
+            cv2.circle(frame, pt, 5, (0, 0, 255), -1)
+            cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
 
-    print("Tracking objects")
-    print(tracking_objects)
+    #print("Tracking objects")
+    #print(tracking_objects)
 
 
-    print("CUR FRAME LEFT PTS")
-    print(center_points_cur_frame)
-
+    #print("CUR FRAME LEFT PTS")
+    #print(center_points_cur_frame)
 
     cv2.imshow("Frame", frame)
 
