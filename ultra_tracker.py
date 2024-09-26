65b02407f34593b037e1bc87bc509f59fba88f7f
Rafael Perazzo <rafael.mota@ufca.edu.br>
2024-09-26 16:05:31 -0300

    fix: corrigida utilização de gpu

--- ultra_tracker.py
+++ ultra_tracker.py
@@ -5,7 +5,12 @@ import cv2
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
 
+import torch
 import configparser
+
+#import os
+
+#os.environ['YOLO_VERBOSE'] = 'True'
 
 config = configparser.ConfigParser()
 try:
@@ -13,17 +18,26 @@ try:
     CUT_IMAGE = int(config['DEFAULT']['CUT_IMAGE'])
     CUT_IMAGE_V = int(config['DEFAULT']['CUT_IMAGE_V'])
     DISTANCIA = int(config['DEFAULT']['DISTANCIA'])
+    VERBOSE = bool(int(config['DEFAULT']['VERBOSE']))
+    if torch.cuda.is_available():
+        CUDA = True
+    else:
+        CUDA = False
 except Exception as e:
     CUT_IMAGE = 3 #Quanto menor, menos corta a imagem. Ex: 2 corta pela metade. 3 corta 1/3 da imagem.
     CUT_IMAGE_V = 1
-    DISTANCIA = 80 
+    DISTANCIA = 80
+    VERBOSE = False
+    CUDA = False
+    print(e)
 
 # Dictionary to store tracking history with default empty lists
 track_history = defaultdict(lambda: [])
 
 # Load the YOLO model with segmentation capabilities
-model = YOLO("yolov8n-seg.pt")
-
+model = YOLO("yolov8n-seg.pt",verbose=VERBOSE)
+if CUDA:
+    model.to('cuda')
 # Open the video file
 cap = cv2.VideoCapture("cars2.mp4")
 
@@ -32,7 +46,7 @@ w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FR
 
 # Initialize video writer to save the output video with the specified properties
 out = cv2.VideoWriter("instance-segmentation-object-tracking.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (w, h))
-
+print(VERBOSE)
 while True:
     # Read a frame from the video
     ret, im0 = cap.read()
@@ -44,16 +58,22 @@ while True:
     annotator = Annotator(im0, line_width=2)
 
     # Perform object tracking on the current frame
-    results = model.track(im0, persist=True)
+    results = model.track(im0, persist=True,verbose=VERBOSE)
     
     # Check if tracking IDs and masks are present in the results
     if results[0].boxes.id is not None and results[0].masks is not None:
         # Extract masks and tracking IDs
         masks = results[0].masks.xy
-        track_ids = results[0].boxes.id.int().cpu().tolist()
-        boxes = results[0].boxes.xywh.cpu()
-        names = results[0].names
-        classes = results[0].boxes.cls.int().cpu().tolist()
+        if CUDA:
+            track_ids = results[0].boxes.id.int().cuda().tolist()
+            boxes = results[0].boxes.xywh.cuda()
+            names = results[0].names
+            classes = results[0].boxes.cls.int().cuda().tolist()
+        else:
+            track_ids = results[0].boxes.id.int().cpu().tolist()
+            boxes = results[0].boxes.xywh.cpu()
+            names = results[0].names
+            classes = results[0].boxes.cls.int().cpu().tolist()
         # Annotate each mask with its corresponding tracking ID and color
         for mask, track_id,box,classe in zip(masks, track_ids,boxes,classes):
             x,y,w,h = box
@@ -65,7 +85,7 @@ while True:
                 if (len(lasts) > 1):
                     x1, y1 = lasts[-2]
                     x2, y2 = lasts[-1]
-                    if (y1-y2 <= 0):
+                    if (y1-y2 < 0):
                         annotator.seg_bbox(mask=mask, mask_color=colors(track_id, True), label=str(track_id))
             
     # Write the annotated frame to the output video
