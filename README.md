75282bb831be46393bcadcaf1ad43d1caae38f4f
Rafael Mota <rafael.mota@ufca.edu.br>
2024-09-27 10:04:12 -0300

    doc: readme atualizado

--- README.md
+++ README.md
@@ -6,9 +6,7 @@ Projeto para identificação de infrações, a partir de vídeos de vias públic
 git clone https://github.com/rafaelperazzo/vehicle_tracker.git
 cd vehicle_tracker
 sudo apt-get install python3-pip cmake
-pip3 install virtualenv
-virtualenv python
-source python/bin/activate
 pip install -r requirements.txt
-wget -O dnn_model/yolov4.weights https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights&ved=2ahUKEwj9mbKrwtuIAxWvqZUCHVlXPE8QFnoECBQQAQ&usg=AOvVaw30if4joxtTaS8DAh12vYQ4
 ```
+
+- Testado no Python 3.8+
--- ultra_tracker.py
+++ ultra_tracker.py
@@ -39,14 +39,14 @@ model = YOLO("yolov8n-seg.pt",verbose=VERBOSE)
 if CUDA:
     model.to('cuda')
 # Open the video file
-cap = cv2.VideoCapture("cars2.mp4")
+cap = cv2.VideoCapture("./videos/cars2.mp4")
 
 # Retrieve video properties: width, height, and frames per second
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
 # Initialize video writer to save the output video with the specified properties
 out = cv2.VideoWriter("instance-segmentation-object-tracking.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (w, h))
-print(VERBOSE)
+
 while True:
     # Read a frame from the video
     ret, im0 = cap.read()
